e8591f67ce600b80f13d077d368813c83f0814c4
wilhelmusfao <104365841+xzallator@users.noreply.github.com>
2025-11-09 20:09:49 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
+import 'screens/login_page.dart';
+import 'screens/home_page.dart';
+import 'screens/cekdetail_page.dart';
+import 'screens/profile_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Aplikasi Sewa Mobil',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+      routes: {
+        '/home': (context) => const HomePage(),
+        '/detail': (context) => const MobilDetailPage(
+              namaMobil: '',
+              harga: '',
+            ),
+        '/profile': (context) => const ProfilePage(),
+      },
+    );
+  }
+}
 
